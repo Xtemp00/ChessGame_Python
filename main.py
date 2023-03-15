568b098cab8250ad6f4f939097f89df1435d6a3e
Benjamin rondo <b.rondo@orange.fr>
2023-03-15 21:24:47 +0100

    premier commit et début d'un test visant a tester la difference de code entre un jeu d'echecs en java et un jeu d'échecs en python

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+# Initialisation de Pygame
+pygame.init()
+
+# Définition des constantes
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 640
+SQUARE_SIZE = SCREEN_WIDTH // 8
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+
+
+
