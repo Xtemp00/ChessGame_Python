97a50cc876e4dbc11de286dd9feaaeccd3fa6c29
ryanl <ryanloureau@gmail.com>
2023-04-05 18:05:41 +0200

    Création d'un fichier pour créer une database

---
+++ Databases.py
@@ -0,0 +1,23 @@
+#Fichier permettant de créer la Database d'un jeu d'echec
+# Il y aura 3 tables:
+# - La table des joueurs :
+#   - Id_Joueurs
+#   - Nom_Joueurs
+#   - nb_parties
+#   - nb_victoires
+#   - nb_defaites
+#   - nb_nulles
+# - La table des parties :
+#   - Id_Partie
+#   - Id_Joueur
+#   - Id_Adversaire
+#   - Resultat
+
+# - La table des mouvements :
+#   - Id_Partie
+#   - Id_Joueur
+#   - Id_Adversaire
+#   - Mouvement
+
+
+# Importation des modules
